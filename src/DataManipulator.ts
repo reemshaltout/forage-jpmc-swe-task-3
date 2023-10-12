ed7cad5a68414768bf9d78797eea91cdf89e5cfe
reem.shaltout <reem.shaltout@fdmgroup.com>
2023-10-12 22:43:37 +0100

    Re-add sample unit test

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -11,22 +11,23 @@ export interface Row {
 }
 
 
-export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]): Row {
-    const priceABC = (serverRespond[0].top_ask_price+ serverRespond[0].top_bid.price)/2;
-    const priceDEF = (serverRespond[1].top_ask_price+ serverRespond[1].top_bid.price)/2;
-    const ratio = priceABC/priceDEF;
-    const upperBound = 1 + 0.05;
-    const lowerBound = 1 - 0.05;
-    return {
-        price_abc: priceABC,
-        price_def: priceDEF,
-        ratio,
-        timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
-            serverRespond[0].timestamp : serverRespond[1].timestamp,
-        upper_bound: upperBound,
-        lower_bound: lowerBound,
-        trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
-    };
-  }
+ export class DataManipulator {
+    static generateRow(serverRespond: ServerRespond[]): Row {
+        const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+        const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+        const ratio = priceABC / priceDEF;
+        const upperBound = 1 + 0.05;
+        const lowerBound = 1 - 0.05;
+        return {
+            price_abc: priceABC,
+            price_def: priceDEF,
+            ratio,
+            timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+                serverRespond[0].timestamp : serverRespond[1].timestamp,
+            upper_bound: upperBound,
+            lower_bound: lowerBound,
+            trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+        };
+    }
+ }
 
