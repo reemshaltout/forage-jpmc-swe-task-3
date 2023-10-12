0c120214646799233d3b86c2ad148d4cc068b4f9
reem.shaltout <reem.shaltout@fdmgroup.com>
2023-10-12 22:37:43 +0100

    Re-add sample unit test

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -27,5 +27,6 @@ export class DataManipulator {
         upper_bound: upperBound,
         lower_bound: lowerBound,
         trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
-  };
-}
+    };
+  }
+
